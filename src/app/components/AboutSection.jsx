93943eb4b889f5c4051141f904a490bbc61da5c9
Jordan Wooley <47928010+jordanwooley95@users.noreply.github.com>
2024-11-20 15:16:39 -0500

    update

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -54,7 +54,7 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="min-h-screen text-white" id="about">
+    <section className=" text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/about-image.png"
